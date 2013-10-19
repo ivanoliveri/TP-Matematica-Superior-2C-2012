d9ace73efd2b1c2e7bb6c1e316e6f75957368eba
ivanoliveri <ipoliveri@gmail.com>
2013-10-19 03:17:36 -0300

    Agregados Formulario Principal

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GUI
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtXValue = new System.Windows.Forms.TextBox();
+            this.txtYValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddOrdenedPair = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDeleteSelectedOrdenedPair = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddOrdenedPair);
+            this.groupBox1.Controls.Add(this.txtYValue);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtXValue);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 95);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingreso de Pares Ordenados";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor de X :";
+            // 
+            // txtXValue
+            // 
+            this.txtXValue.Location = new System.Drawing.Point(75, 28);
+            this.txtXValue.Name = "txtXValue";
+            this.txtXValue.Size = new System.Drawing.Size(100, 20);
+            this.txtXValue.TabIndex = 1;
+            // 
+            // txtYValue
+            // 
+            this.txtYValue.Location = new System.Drawing.Point(271, 28);
+            this.txtYValue.Name = "txtYValue";
+            this.txtYValue.Size = new System.Drawing.Size(100, 20);
+            this.txtYValue.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor de Y :";
+            // 
+            // btnAddOrdenedPair
+            // 
+            this.btnAddOrdenedPair.Location = new System.Drawing.Point(7, 55);
+            this.btnAddOrdenedPair.Name = "btnAddOrdenedPair";
+            this.btnAddOrdenedPair.Size = new System.Drawing.Size(364, 31);
+            this.btnAddOrdenedPair.TabIndex = 4;
+            this.btnAddOrdenedPair.Text = "Agregar";
+            this.btnAddOrdenedPair.UseVisualStyleBackColor = true;
+            this.btnAddOrdenedPair.Click += new System.EventHandler(this.btnAddOrdenedPair_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDeleteSelectedOrdenedPair);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 114);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(378, 195);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Valores Ingresados";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(364, 128);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnDeleteSelectedOrdenedPair
+            // 
+            this.btnDeleteSelectedOrdenedPair.Location = new System.Drawing.Point(9, 155);
+            this.btnDeleteSelectedOrdenedPair.Name = "btnDeleteSelectedOrdenedPair";
+            this.btnDeleteSelectedOrdenedPair.Size = new System.Drawing.Size(364, 31);
+            this.btnDeleteSelectedOrdenedPair.TabIndex = 5;
+            this.btnDeleteSelectedOrdenedPair.Text = "Eliminar Par Ordenado Seleccionado";
+            this.btnDeleteSelectedOrdenedPair.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(13, 316);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(377, 96);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Resultados";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Polinomio Progresivo :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Polinomio Regresivo :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(124, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(246, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(400, 424);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TP-Matematica-Superior-2C-2012";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAddOrdenedPair;
+        private System.Windows.Forms.TextBox txtYValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtXValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnDeleteSelectedOrdenedPair;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -6,14 +6,34 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Domain;
 
 namespace GUI
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+
+        public List<OrdenedPair> ordenedPairs { set; get; }
+
+        public frmMain()
         {
+            ordenedPairs = new List<OrdenedPair>();
+
             InitializeComponent();
+            
+        }
+
+        private void btnAddOrdenedPair_Click(object sender, EventArgs e)
+        {
+            var newOrdenedPair = new OrdenedPair()
+            {
+                xValue = int.Parse(txtXValue.Text),
+                yValue = int.Parse(txtYValue.Text)
+            };
+
+            ordenedPairs.Add(newOrdenedPair);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = ordenedPairs;
         }
     }
 }
